d3a26afd083e9d20c004e786ba3cd21752177b4d
Kaitlyn Childers <kac0043@auburn.edu>
2018-02-10 00:44:39 -0600

    dates are a thing

--- microservice.py
+++ microservice.py
@@ -11,12 +11,43 @@ def getCurrentData():
     location = queryGoogleGeocode(request.args.get('location'))
     if (location.startswith('error')):
         return location
-    jsonResponse = queryDarkSkyForecast(location)
+    date = str(request.args.get('date'))
+    print(date)
+    if (date):
+        hour = time.gmtime().tm_hour
+        minu = time.gmtime().tm_min
+        sec = time.gmtime().tm_sec
+        if (hour < 10):
+            h = '0' + str(hour)
+        else: 
+            h = str(hour)
+        if (minu < 10):
+            m = '0' + str(minu)
+        else: 
+            m = str(minu)
+        if (sec < 10):
+            s = '0' + str(sec)
+        else: 
+            s = str(sec)
+        date = date + 'T' + h + ':' + m + ':' + s +'Z'
+        print(date)
+        jsonResponse = queryDarkSkyTimeMachine(location, date)
+    else:
+        jsonResponse = queryDarkSkyForecast(location)
     data = json.loads(jsonResponse)
     return data['currently']['summary']
 
 def queryDarkSkyForecast(latLongString):
     request = 'https://api.darksky.net/forecast/4a21043802f4364273e7fe25ba29c92b/'+latLongString
+    try:
+	    response = urlopen(request)
+	    return response.read()
+    except:
+        return 'error'
+
+def queryDarkSkyTimeMachine(latLongString, time):
+    request = 'https://api.darksky.net/forecast/4a21043802f4364273e7fe25ba29c92b/'+latLongString+','+time
+    print(request)
     try:
 	    response = urlopen(request)
 	    return response.read()
